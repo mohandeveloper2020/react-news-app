a674828a174c27ad1e2ed2c53122b458c926e51f
Mohan M <mohandeveloper2020@gmail.com>
2018-06-28 19:15:13 +0530

    styling changes

--- src/components/container/NewsApple/index.js
+++ src/components/container/NewsApple/index.js
@@ -10,8 +10,8 @@ class NewsApple extends Component {
   componentDidMount(){
 
     const apple = `https://newsapi.org/v2/everything?q=apple&from=2018-06-20&to=2018-06-20&sortBy=popularity&apiKey=6c13a32e23b643989e19a722674ca873`;
-    const techcrunch = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=6c13a32e23b643989e19a722674ca873`;
-    const us = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6c13a32e23b643989e19a722674ca873`;
+    //const techcrunch = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=6c13a32e23b643989e19a722674ca873`;
+    //const us = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6c13a32e23b643989e19a722674ca873`;
 
     fetch(`${apple}`)
       .then(
