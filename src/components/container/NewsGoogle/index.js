9d8bace0c51d321de354c05ecdf817e9c5bb5a9e
Mohan Madheshwaran <MMADHESH@global.sial.com>
2019-01-04 22:23:52 +0530

    Used Promise to fetch multiple apis at once

--- src/components/container/NewsGoogle/index.js
+++ src/components/container/NewsGoogle/index.js
@@ -7,40 +7,60 @@ class NewsGoogle extends Component {
     newsIndia : [],
     newsUk : []
   }
-
+  
   componentDidMount(){
 
     const googleIndia = `https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=6c13a32e23b643989e19a722674ca873`;
     const googleUk = `https://newsapi.org/v2/top-headlines?sources=google-news-uk&apiKey=6c13a32e23b643989e19a722674ca873`;
-    //const us = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6c13a32e23b643989e19a722674ca873`;
 
-    fetch(`${googleIndia}`)
+    /* Used Promise to fetch multiple api's at once. */
+    Promise.all( [fetch(`${googleIndia}`), fetch(`${googleUk}`)] )
       .then(
-        (response) => {
-          //console.log(response)
-          return response.json()
+        ([response1, response2]) => { 
+          //console.log(response1, response2)
+          return Promise.all( [response1.json(), response2.json()] ) 
         }
       )
       .then(
         (json) => {
-          console.log(json.articles)
-          this.setState({ newsIndia : json.articles })
+          //console.log(json);
+          this.setState({
+            newsIndia : json[0].articles,
+            newsUk : json[1].articles
+          })
         }
       )
 
-      fetch(`${googleUk}`)
-      .then(
-        (response) => {
-          //console.log(response)
-          return response.json()
-        }
-      )
-      .then(
-        (json) => {
-          console.log(json.articles)
-          this.setState({ newsUk : json.articles })
-        }
-      )
+
+    /* Fetching api's separately */
+
+    // fetch(`${googleIndia}`)
+    //   .then(
+    //     (response) => {
+    //       console.log(response)
+    //       return response.json()
+    //     }
+    //   )
+    //   .then(
+    //     (json) => {
+    //       console.log(json.articles)
+    //       this.setState({ newsIndia : json.articles })
+    //     }
+    //   )
+
+    //   fetch(`${googleUk}`)
+    //   .then(
+    //     (response) => {
+    //       //console.log(response)
+    //       return response.json()
+    //     }
+    //   )
+    //   .then(
+    //     (json) => {
+    //       // console.log(json.articles)
+    //       this.setState({ newsUk : json.articles })
+    //     }
+    //   )
   }
 
   render(){
